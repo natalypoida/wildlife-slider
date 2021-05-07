7752574deef70a90ab06f2e42b60eb985ccbef5d
Natalya POida <nataly.poida@gmail.com>
2021-05-07 20:31:06 +0300

    feat: add basic breakpoints for slider

--- assets/index.js
+++ assets/index.js
@@ -4,5 +4,31 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
     slidesPerView: 3,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 1,
+
+
+        },
+        960: {
+            slidesPerView: 2,
+
+        },
+        1200: {
+            slidesPerView: 3,
+
+        },
+    },
 })
 swiper.init();
